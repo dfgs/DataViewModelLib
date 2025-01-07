e6fdfc445f32664552fc7c364410ec084b395d38
dfgs <dfgs83@gmail.com>
2025-01-07 17:17:42 +0100

    building test ui

--- TestGUI/MainWindow.xaml.cs
+++ TestGUI/MainWindow.xaml.cs
@@ -8,9 +8,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using TestGUI.Data.DataSources.ViewModels;
-using TestGUI.Data.DataSources.Models;
-using TestGUI.Data.DataSources;
+
 
 namespace TestGUI
 {
@@ -19,7 +17,6 @@ namespace TestGUI
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		private TestDatabaseViewModel testDatabaseViewModel;
 
 
 
@@ -27,32 +24,11 @@ namespace TestGUI
 		{
 			InitializeComponent();
 
-			TestDatabase testDatabase = new TestDatabase();
-
-			testDatabase.PersonnTable.Add(new Personn(1, "Homer", "Simpson", 40) { DeliveryAddressID = 1, BillingAddressID = 2, PetID = 1 });
-			testDatabase.PersonnTable.Add(new Personn(2, "Marje", "Simpson", 40) { DeliveryAddressID = 1, BillingAddressID = 2, PetID = 1 });
-			testDatabase.PersonnTable.Add(new Personn(3, "Bart", "Simpson", 10) { DeliveryAddressID = 1, PetID = 2 });
-			testDatabase.PersonnTable.Add(new Personn(4, "Liza", "Simpson", 9) { DeliveryAddressID = 1, PetID = 2 });
-
-			testDatabase.AddressTable.Add(new Address(1, "Home") { Number = 123 });
-			testDatabase.AddressTable.Add(new Address(2, "School") { Number = 44 });
-			testDatabase.AddressTable.Add(new Address(3, "Work") { Number = 55 });
-
-			testDatabase.PetTable.Add(new Pet(1, "Cat"));
-			testDatabase.PetTable.Add(new Pet(2, "Dog"));
-			testDatabase.PetTable.Add(new Pet(3, "Turtle"));
-
-			testDatabaseViewModel = new TestDatabaseViewModel(new TestDatabaseModel(testDatabase));
-			this.DataContext= testDatabaseViewModel;
+			
+			//this.DataContext= Data.TestDatabaseViewModel;
 		}
 
-		private void PersonnCollectionView_Insert(object sender, InsertEventArg e)
-		{
-			byte id;
-			id = (byte)( testDatabaseViewModel.PersonnViewModelCollection.Select(item => item.PersonnID).Max() + 1);
-			e.Item = new Personn( id, "First name", "Last name", 30);
-			e.Canceled = false;
-        }
+		
 
     }
 }
---
+++ TestGUI/PersonnView.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TestGUI.Data.DataSources.ViewModels;
+using TestGUI.Data.DataSources.Models;
+using TestGUI.Data.DataSources;
+
+namespace TestGUI
+{
+	/// <summary>
+	/// Logique d'interaction pour PersonnView.xaml
+	/// </summary>
+	public partial class PersonnView : UserControl
+	{
+		public PersonnView()
+		{
+			InitializeComponent();
+		}
+
+		private void CollectionView_Insert(object sender, InsertEventArg e)
+		{
+			byte id;
+			id = (byte)(ViewModels.TestDatabaseViewModel.PersonnViewModelCollection.Select(item => item.PersonnID).Max() + 1);
+			e.Item = new Personn(id, "First name", "Last name", 30);
+			e.Canceled = false;
+		}
+
+
+	}
+}
