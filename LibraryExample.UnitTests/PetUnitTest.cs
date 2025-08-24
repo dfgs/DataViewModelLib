8242389b82dce028c5be5e186a9677d3574272f4
dfgs <dfgs83@gmail.com>
2025-08-24 15:45:55 +0200

    Refactored unit tests

--- LibraryExample.UnitTests/PetUnitTest.cs
+++ LibraryExample.UnitTests/PetUnitTest.cs
@@ -28,68 +28,6 @@ namespace LibraryExample.UnitTests
 		}
 
 
-		
-
-		
-		
-		[TestMethod]
-		public void ShouldGetOwners()
-		{
-			TestDatabaseModel testDatabaseModel;
-			TestDatabaseViewModel testDatabaseViewModel;
-			PersonnViewModel[] owners;
-
-			testDatabaseModel = new TestDatabaseModel(Utils.CreateTestDatabase());
-			testDatabaseViewModel = new TestDatabaseViewModel(testDatabaseModel);
-
-			owners = testDatabaseViewModel.PetViewModelCollection.ElementAt(0).Owners.ToArray();
-			Assert.AreEqual(2, owners.Length);
-			Assert.AreEqual("Homer", owners[0].FirstName);
-			Assert.AreEqual("Marje", owners[1].FirstName);
-
-			owners = testDatabaseViewModel.PetViewModelCollection.ElementAt(1).Owners.ToArray();
-			Assert.AreEqual(2, owners.Length);
-			Assert.AreEqual("Bart", owners[0].FirstName);
-			Assert.AreEqual("Liza", owners[1].FirstName);
-		}
-
-		[TestMethod]
-		public void ShouldGetSetSelectedItem()
-		{
-			TestDatabaseModel testDatabaseModel;
-			TestDatabaseViewModel testDatabaseViewModel;
-			string? propertyName = null;
-
-			testDatabaseModel = new TestDatabaseModel(Utils.CreateTestDatabase());
-			testDatabaseViewModel = new TestDatabaseViewModel(testDatabaseModel);
-
-			testDatabaseViewModel.PetViewModelCollection.PropertyChanged += (_, e) => { propertyName = e.PropertyName; };
-
-			Assert.IsNull(testDatabaseViewModel.PetViewModelCollection.SelectedItem);
-			testDatabaseViewModel.PetViewModelCollection.SelectedItem = testDatabaseViewModel.PetViewModelCollection.First();
-			Assert.IsNotNull(testDatabaseViewModel.PetViewModelCollection.SelectedItem);
-			Assert.AreEqual("Cat", testDatabaseViewModel.PetViewModelCollection.SelectedItem.Name);
-			Assert.AreEqual("SelectedItem", propertyName);
-		}
-
-		[TestMethod]
-		public void ShouldCreateViewModelProperties()
-		{
-			TestDatabaseModel testDatabaseModel;
-			TestDatabaseViewModel testDatabaseViewModel;
-			IViewModelProperty[] properties;
-
-			testDatabaseModel = new TestDatabaseModel(Utils.CreateTestDatabase());
-			testDatabaseViewModel = new TestDatabaseViewModel(testDatabaseModel);
-
-			properties = testDatabaseViewModel.PetViewModelCollection.First().Properties.ToArray();
-
-			Assert.AreEqual(3, properties.Length);
-			Assert.AreEqual("Pet id", properties[0].Name);
-			Assert.AreEqual("Name", properties[1].Name);
-			Assert.AreEqual("IsValid", properties[2].Name);
-		}
-
 
 	}
 }
